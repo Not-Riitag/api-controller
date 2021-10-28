ab23383eebe73064bcc392dd0bf9d3ddaaaa865a
Matthew <matthew.elswick@matthe815.dev>
2021-10-28 05:22:49 -0400

    slight readabilty updates

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require ('express')
 const {UserManager, SessionManager} = require('./data-controller/index')
 const app = express()
 const passport = require('passport')
-const passportLocal = require('passport-local').Strategy
+const localStrategy = require('passport-local').Strategy
 app.use(express.json())
 app.use(passport.initialize())
 
-passport.use(new passportLocal((username, password, done) => {
+passport.use(new localStrategy((username, password, done) => {
     UserManager.getUserLogin(username, password).then((session) => {
         if (!session) return done(null, false)
         
@@ -25,7 +25,9 @@ app.post('/login', passport.authenticate('local', { session: false }), (req, res
 
 app.post('/register', (req, res) => {
     if (!req.body.username || !req.body.password || !req.body.email) return res.status(400).json({error: 'Missing username, password or email'})
+
     UserManager.createUser(req.body).then((user) => { 
+        if (user == null) return res.json({ error: 'User already exists' })
         res.json(user)
     })
 })
@@ -34,16 +36,12 @@ app.get('/users/:user', async (req, res) => {
     if (req.params.user == "me") {
         const session = await SessionManager.getSession(req.headers.authorization)
         if (!session) return res.status(401).json({error: 'Unauthorized'})
-        const user = await UserManager.getUser({ id: session.user }, { password: 0 })
-        return res.json(user)
+        
+        return res.json(await UserManager.getUser({ id: session.user }, { password: 0 }))
     }
     
     const user = await UserManager.getUser({ id: req.params.user }, {username: 1, id: 1})
-    console.log(user)
-    if (user == null)
-        return res.status(404).json({error: 'User not found'})
-    else
-       return res.json(user)
+    return user == null ? res.status(404).json({error: 'User not found'}) : res.json(user)
 })
 
 app.listen(8091)
