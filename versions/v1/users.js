ebb376d867bac60edac37144ebd60997faed14c5
Matthew <matthew.elswick@matthe815.dev>
2021-10-31 05:01:34 -0400

    Add PATCH endpoint to users alongside validation

--- versions/v1/users.js
+++ versions/v1/users.js
@@ -1,7 +1,9 @@
 const ApiRoute = require("../../API/ApiRoute")
 const Errors = require("../../API/Errors")
 const { SessionManager, UserManager } = require('../../data-controller/index')
-const {  } = require('rest-api-errors')
+const { ConflictError } = require('rest-api-errors')
+const PasswordUtils = require("../../data-controller/src/PasswordUtils")
+const EnumPermissions = require("../../data-controller/src/Enum/EnumPermissions")
 
 module.exports = new ApiRoute({
     route: 'users/:user',
@@ -28,8 +30,31 @@ module.exports = new ApiRoute({
     patch: async (req, res) => {
         const session = await SessionManager.ParseAuthorization(req.headers.authorization)
         if (!session) return res.status(Errors.UnauthorizedError.status).json(Errors.UnauthorizedError)
+        
         const user = await UserManager.getUser({ id: session.user }, { password: 0 })
 
-        res.json({})
+        if (req.params.user == '@me') req.params.user = user.id // Replace the paramater with the current user.
+
+        const updateUser = await UserManager.getUser({ id: req.params.user }, { password: 0 })
+
+        // Do not allow the user to edit other users without permission
+        if ((!user.permissions.has(EnumPermissions.EDIT_USERS) && updateUser.id != user.id)) return res.status(Errors.ForbiddenError.status).json(Errors.ForbiddenError)
+
+        if (req.body.password) {
+            req.body.password = PasswordUtils.makePassword(req.body.password, user.username) // Rehash and and apply the password.
+            SessionManager.removeSession(user) // Remove the current session.
+        }
+
+        if (req.body.username || req.body.email) // Confirm the new username or email is not already in use.
+            if (await UserManager.getUser({ $or: [{ $text: { $search: req.body.username, $caseSensitive: false } }, { email: req.body.email }] }, { id: 1 })) 
+                return res.status(new ConflictError().status).json({ error: 'Username or email already in use' })
+    
+        // Verify if the user is allowed to change the data
+        if (req.body.permissions)
+            // If the permissions don't match, error.
+            if (!user.permissions.has(EnumPermissions.EDIT_USER_PERMISSIONS) || !(user.permissions.has(req.body.permissions))) return res.status(Errors.ForbiddenError.status).json(Errors.ForbiddenError)
+
+        updateUser.update(req.body)
+        res.json({ user: session.user, set: !req.body.password ? req.body : Object.assign({}, req.body, { password: '<redacted>' }) }) // Return the edited fields but redact any password.
     }
 })
